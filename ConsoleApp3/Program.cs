ea49ce4173654734b3ed8ebdba55ee4bed0a482e
jeived2077 <maksim.sarckisian@yandex.ru>
2023-05-03 08:18:40 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+class Circle
+{
+    int x;
+    int y;
+    int radius;
+    public Circle()
+    {
+        x = 0;
+        y = 0;
+        radius = 0;
+    }
+    public Circle(int radius)
+    {
+        x = 0; y = 0;
+        this.radius = radius;
+    }
+    public Circle(int x,int y)
+    {
+      this.x = x;
+        this.y = y;
+        radius = 0;
+    }
+    public Circle(int radius,int x,int y) { 
+    this.radius = radius;
+        this.x = x;
+        this.y = y; 
+    }
+}
